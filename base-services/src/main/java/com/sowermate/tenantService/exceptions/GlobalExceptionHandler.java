dbc8a0d1e0144fdd6c9014eae98ed5350c3d6d14
Suhas Borage <suhas.borage@sowermate.com>
2024-01-18 12:14:37 +0530

    [#86]: Fixing exception handler

---
+++ base-services/src/main/java/com/sowermate/tenantService/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.sowermate.tenantService.exceptions;
+
+
+import com.sowermate.tenantService.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<ApiResponse> handlerResourceNotFoundException( ResourceNotFoundException ex){
+    String message=ex.getMessage();
+      ApiResponse response= ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
+      return new ResponseEntity<ApiResponse>(response,HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiResponse> handlerException( Exception ex){
+    String message=ex.getMessage();
+    ex.printStackTrace();
+    ApiResponse response= ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
+    return new ResponseEntity<ApiResponse>(response,HttpStatus.NOT_FOUND);
+  }
+
+}
